93e110b43c6a9c12a4e5c5ec3d4f38fa4a4fd8b1
SmilingCode <ericc_j@icloud.com>
2019-10-16 01:24:12 +1100

    done server side development

--- server/src/api/messages.js
+++ server/src/api/messages.js
@@ -1,13 +1,51 @@
 const express = require('express');
+const Joi = require('joi');
+
+const db = require('../db');
+const messages = db.get('messages');
+
+const schema = Joi.object().keys({
+  name: Joi.string()
+        .min(3)
+        .max(100)
+        .required(),
+  message: Joi.string()
+          .min(3)
+          .max(500)
+          .required(),
+  latitude: Joi.number().min(-90).max(90).required(),
+  longitude: Joi.number().min(-180).max(180).required()
+})
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.json([]);
+  messages.find({})
+  .then(allMsgs => {
+    res.json(allMsgs);
+  })
 });
 
-router.post('/', (req, res) => {
-  res.json([]);
+router.post('/', (req, res, next) => {
+  // add current time
+  const result = Joi.validate(req.body, schema);
+  if (result.error === null) {
+    const userMessage = {
+      name: req.body.name,
+      message: req.body.message,
+      latitude: req.body.latitude,
+      longitude: req.body.longitude,
+      date: new Date()
+    }
+
+    messages.insert(userMessage)
+    .then(insertedMsg => {
+      res.json(insertedMsg);
+    })
+  } else {
+    next(result.error);
+  }
+  
 });
 
 module.exports = router;
--- server/test/api.test.js
+++ server/test/api.test.js
@@ -14,12 +14,31 @@ describe('GET /api/v1', () => {
   });
 });
 
-describe('GET /api/v1/emojis', () => {
-  it('responds with a json message', function(done) {
+describe('POST /api/v1/messages', () => {
+  it('response with inserted message', function(done) {
+    const sendMsg = {
+      name: 'Jon',
+      message: 'i do not know what is wrong',
+      latitude: '-20',
+      longitude: '115'
+    };
+
+    const recMsg = {
+      ...sendMsg,
+      _id: '5da5bd1b8a010d9db7bedf98',
+      date: '2019-10-15T12:44:03.063Z'
+    }
+
     request(app)
-      .get('/api/v1/emojis')
+      .post('/api/v1/messages')
+      .send(sendMsg)
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
-      .expect(200, ['😀', '😳', '🙄'], done);
+      .expect(res => {
+        console.log(res.body)
+        res.body._id = '5da5bd1b8a010d9db7bedf98',
+        res.body.date = '2019-10-15T12:44:03.063Z'
+      })
+      .expect(200, recMsg, done);
   });
 });
